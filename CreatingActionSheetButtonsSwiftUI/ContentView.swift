e0e0e00a34bcb46d2bf7a746e84caa60bb3e8202
Sandy Gomez <sgomez1029@gmail.com>
2024-02-28 21:02:05 -0800

    Creating Action Sheet Buttons in SwiftUI (UCSD Extension

--- CreatingActionSheetButtonsSwiftUI/ContentView.swift
+++ CreatingActionSheetButtonsSwiftUI/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showActionSheet = false
+    @State private var buttonTapped = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 25){
+            Text("\(buttonTapped)")
+            Button(action: {
+                showActionSheet.toggle()
+            }) {
+                Text("Show Alert")
+            }
+            .actionSheet(isPresented: $showActionSheet) {
+                ActionSheet(title: Text("Look at all the options on an action sheet!"), message: nil, buttons: [
+                    .default(Text("Default"), action: { buttonTapped = "Default"
+                    }),
+                    .default(Text("Another Default"), action: {
+                        buttonTapped = "Another Default"
+                    }),
+                    .destructive(Text("Destructive"), action: {
+                        buttonTapped = "Destructive"
+                    }),
+                    .cancel(Text("Cancel"), action: {
+                        buttonTapped = "Cancelled"
+                    })
+                ]
+                )
+            }
         }
-        .padding()
     }
 }
 
